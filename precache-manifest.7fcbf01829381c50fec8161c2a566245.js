b9dcc1ed3639d9ec6ef86d7d045dc04bb0473345
Olujuwon Dare <dareolujuwon@gmail.com>
2018-11-21 16:32:49 -0500

    Updates

---
+++ precache-manifest.7fcbf01829381c50fec8161c2a566245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "efba9e98601ff23dae4e",
+    "url": "/tripsapp-frontend/static/css/main.81738f9a.chunk.css"
+  },
+  {
+    "revision": "efba9e98601ff23dae4e",
+    "url": "/tripsapp-frontend/static/js/main.efba9e98.chunk.js"
+  },
+  {
+    "revision": "48bd4d7550d38863c487",
+    "url": "/tripsapp-frontend/static/css/1.9a9b3b14.chunk.css"
+  },
+  {
+    "revision": "48bd4d7550d38863c487",
+    "url": "/tripsapp-frontend/static/js/1.48bd4d75.chunk.js"
+  },
+  {
+    "revision": "5e0e4104584e1b4829a6",
+    "url": "/tripsapp-frontend/static/js/runtime~main.5e0e4104.js"
+  },
+  {
+    "revision": "bb93fff5d81ce4ec3bbde842a916d588",
+    "url": "/tripsapp-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tripsapp/precache-manifest.bb43887f6c3cd06a0db73e4ead99cb0d.js"
+  "/tripsapp-frontend/precache-manifest.7fcbf01829381c50fec8161c2a566245.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tripsapp/index.html", {
+workbox.routing.registerNavigationRoute("/tripsapp-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
